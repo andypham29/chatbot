c1838df8d53997a54b60bcf257d2ed250f0a7c2b
andypham29 <andy.pham2956@gmail.com>
2018-10-16 23:54:46 -0400

    init project

---
+++ index.js
@@ -0,0 +1,27 @@
+'use strict'
+
+const express = require('express');
+const bodyParser = require('body-parser');
+const requrest = require('request');
+
+const app = express('port', (process.env.PORT || 5000));
+
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
+
+//routes
+app.get('/', function(req,res){
+    res.send("chatbot")
+});
+
+//facebook handshake
+app.get('/webhook', function(req,res){
+   if(req.query['hub.verify_token'] === "helloworld"){
+       res.send(req.query['hub.challenge']);
+   }
+   res.send("Wrong token");
+});
+
+app.listen(app.get('port'), function(){
+    console.log('chat bot running on: port')
+});
